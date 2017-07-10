28913a55975c1489ee9d37e87910cc9cbcd53905
dpelovski@icloud.com <dpelovski@icloud.com>
2017-07-10 15:33:01 +0300

    small adjucments

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -32,10 +32,10 @@ class Navbar extends Component {
     <aside className="sidebar-left">
         <Link to='/' className="company-logo">Logo</Link>
         <div className="sidebar-links">
-            <Link to="/" className="link-blue" ><i className="fa fa-home"></i>Home</Link>
-            { Auth.isUserAuthenticated() ? (
+        { Auth.isUserAuthenticated() ? (
           <div>
-            <span>Hi, {this.state.username}</span>
+            <h3>Hi, {this.state.username}</h3>
+            <Link to="/" className="link-blue" ><i className="fa fa-home"></i>Home</Link>
             <Link to='/admin' className='link-blue'><i className="fa fa-user"></i>Admin Console</Link>
             <Link to="/users/logout" className="link-red"><i className="fa fa-sign-out"></i>Logout</Link>
             <Link to="/contact" className="link-yellow"><i className="fa fa-phone"></i>Contact</Link>
@@ -43,6 +43,7 @@ class Navbar extends Component {
           </div>
         ) : (
           <div>
+            <Link to="/" className="link-blue" ><i className="fa fa-home"></i>Home</Link>
             <Link to='/users/login'><i className="fa fa-sign-in"></i>Login</Link>
             <Link to="/contact" className="link-yellow"><i className="fa fa-phone"></i>Contact</Link>
             <Link to="/testimonials" className="link-green"><i className="fa fa-map-marker"></i>Testimonials</Link>
