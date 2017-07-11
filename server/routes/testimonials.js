b1609cd177dca1243752602f9afc77b9d83a09ee
awekisa <dimitar.valtchev@gmail.com>
2017-07-11 16:02:29 +0300

    add approved testimonials route on the server

--- server/routes/testimonials.js
+++ server/routes/testimonials.js
@@ -120,6 +120,7 @@ router.post('/add', authCheck, (req, res) => {
 router.get('/all-approved', (req, res) => {
   Testimonial
     .find({})
+    .where('approved').equals(true)
     .then(testimonials => {
       res.status(200).json(testimonials)
     })
