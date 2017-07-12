0c44cc7f7751bea29fd2ac37f716ef68400afae3
dpelovski@icloud.com <dpelovski@icloud.com>
2017-07-12 16:48:00 +0300

    the admin console in the navbar will now show only for logged in admin user

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -36,7 +36,11 @@ class Navbar extends Component {
           <div>
             <h3>Hi, {this.state.username}</h3>
             <Link to="/" className="link-blue" ><i className="fa fa-home"></i>Home</Link>
-            <Link to='/admin' className='link-blue'><i className="fa fa-user"></i>Admin Console</Link>
+            {Auth.isUserAdmin() ? (
+              <Link to='/admin' className='link-blue'><i className="fa fa-user"></i>Admin Console</Link>
+            ) : (
+              <div></div>
+            )}
             <Link to="/users/logout" className="link-red"><i className="fa fa-sign-out"></i>Logout</Link>
             <Link to="/contact" className="link-yellow"><i className="fa fa-phone"></i>Contact</Link>
             <Link to="/testimonials" className="link-green"><i className="fa fa-map-marker"></i>Testimonials</Link>
